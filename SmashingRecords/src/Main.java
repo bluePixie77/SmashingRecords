53f390cac01190c7c2e9658951ebcabe419a6034
bluePixie77 <mariaramisbarrera@gmail.com>
2025-09-26 14:33:38 +0200

    commit

---
+++ SmashingRecords/src/Main.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings(){
+        size(800, 800);
+    }
+    public void setup(){
+
+    }
+    public void draw(){
+
+    }
+    public void keyPressed(){
+
+    }
+    public void mousePressed(){
+
+    }
+}

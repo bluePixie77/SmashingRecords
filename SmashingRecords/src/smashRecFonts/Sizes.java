27b1d12e9eaef94b1c31f9470176d534f781c515
bluePixie77 <mariaramisbarrera@gmail.com>
2025-10-02 08:59:49 +0200

    commit

---
+++ SmashingRecords/src/smashRecFonts/Sizes.java
@@ -0,0 +1,10 @@
+package smashRecFonts;
+
+public class Sizes {
+
+    // static para poder acceder sin un objeto;
+    public static float medidaTitulo = 60;
+    public static float medidaSubtitulo = 60;
+    public static float medidaParagrafo = 60;
+
+}
